e4ad379450c68044d91f752bf58f4f6d3f9220ad
martin-adamski <martin.p.adamski@gmail.com>
2025-09-21 14:37:50 +0100

    initial stages of the ai agent

---
+++ main.py
@@ -0,0 +1,49 @@
+import os
+import sys
+from dotenv import load_dotenv
+from google import genai
+from google.genai import types
+
+
+def main():
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+
+    verbose = "--verbose" in sys.argv
+    args = []
+    for arg in sys.argv[1:]:
+        if not arg.startswith("--"):
+            args.append(arg)
+
+    if not args:
+        print("Prompts not provided")
+        sys.exit(1)
+
+    user_prompt = " ".join(args)
+
+    messages = [
+    types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
+
+    generate_content(client, messages, verbose, user_prompt)
+
+
+def generate_content(client, messages, verbose, user_prompt):
+    response = client.models.generate_content(
+    model='gemini-2.0-flash-001', 
+    contents=messages
+    )
+
+    print("Response:")
+    print(response.text)
+
+    if verbose:
+        print(f"User prompt: {user_prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+
+
+if __name__ == "__main__":
+    main()
